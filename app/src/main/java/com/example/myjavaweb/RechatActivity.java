ec88bafce99adc37bd078b3bbc99a96ffecc0d1a
bae <seongbin5962@daum.net>
2020-12-02 16:06:56 +0900

    reChat and UIControll

--- app/src/main/java/com/example/myjavaweb/RechatActivity.java
+++ app/src/main/java/com/example/myjavaweb/RechatActivity.java
@@ -3,6 +3,10 @@ package com.example.myjavaweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RechatActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class RechatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rechat);
+
+        EditText et = findViewById(R.id.etChatMsg);
+        TextView tv = findViewById(R.id.tvMsg);
+        Button btn = findViewById(R.id.btnChatSend);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = et.getText().toString();
+                tv.append(msg + "\n");
+                et.setText("");
+            }
+        });
     }
 }
