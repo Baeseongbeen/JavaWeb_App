4fc99d968a604b2965561640def5c90f24545a71
bae <seongbin5962@daum.net>
2020-12-03 15:21:44 +0900

    listView2

--- app/src/main/java/com/example/myjavaweb/ListActivity.java
+++ app/src/main/java/com/example/myjavaweb/ListActivity.java
@@ -3,7 +3,9 @@ package com.example.myjavaweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,21 +13,37 @@ import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
+    List<String> items = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    EditText etmsg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        List<String> items = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        etmsg = findViewById(R.id.etMsgEx);
+
+        adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         ListView lv = findViewById(R.id.lvMsg);
         lv.setAdapter(adapter);
 
-        items.add("아이언맨");
 
-        for(int i= 0; i< 50; i++)
-            items.add(i + "");
+        items.add("채팅");
+        findViewById(R.id.btnMsgSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = etmsg.getText().toString().trim();
+                if(msg.equals(""))
+                    return;
+
+                etmsg.setText("");
+                items.add(msg);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
 
     }
 }
